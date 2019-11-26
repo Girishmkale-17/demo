316debfbb10ec30f987061c609e87f81718cf681
girishmkale <girishmkale@gmail.com>
2019-11-26 16:40:14 +0530

    latest file commited

--- demo.js
+++ demo.js
@@ -1,11 +1,6 @@
 console.log("welcome")
 console.log(" hiii")
 console.log("welcome")
-console.log(" hiii")
-console.log("welcome")
-console.log(" hiii")
-console.log("welcome")
-console.log(" hiii")
 console.log("welcome")
 console.log(" i am girish")
 
